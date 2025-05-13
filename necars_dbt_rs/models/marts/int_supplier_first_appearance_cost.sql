192111eefc6a3fd952406310c01c8fe90fc6c134
Donald Futers <donald.futers@gmail.com>
2025-05-13 20:33:46 +0100

    🐛 fix Redshift QUALIFY issue in int_supplier_first_appearance_vehicle by aliasing the preceding FROM clause's referent
    ♻️  refactor int_supplier_first_appearance_cost to employ the same fix, rather than a CTE and WHERE

--- necars_dbt_rs/models/marts/int_supplier_first_appearance_cost.sql
+++ necars_dbt_rs/models/marts/int_supplier_first_appearance_cost.sql
@@ -1,19 +1,13 @@
 -- Get each supplier's first appearance in the cost table
-with ranked_appearances as (
-    select
-        row_number() over(
-            partition by recondition_supplier
-            order by date
-        ) as appearance
-        ,date as recondition_date
-        ,recondition_supplier
-    from
-        {{ ref('stg_source_cost') }}
-)
 select
-    recondition_date
+    row_number() over(
+        partition by recondition_supplier
+        order by date
+    ) as appearance
+    ,date as recondition_date
     ,recondition_supplier
 from
-    ranked_appearances
-where
+    -- Redshift requires a FROM-clause alias when immediately preceding QUALIFY
+    {{ ref('stg_source_cost') }} as stg_source_cost
+qualify
     appearance = 1
