97f37172c4fafc204866099905ba88b900f95691
Donald Futers <donald.futers@gmail.com>
2025-05-15 17:31:19 +0100

    ⚙️  update Airflow/Astro requirements.txt, Dockerfile, and DAG config file for Redshift compatibility

--- necars_dbt_dag/dags/necars_dbt_dag.py
+++ necars_dbt_dag/dags/necars_dbt_dag.py
@@ -2,24 +2,24 @@ import os
 from datetime import datetime
 
 from cosmos import DbtDag, ProfileConfig, ProjectConfig, ExecutionConfig
-from cosmos.profiles import SnowflakeUserPasswordProfileMapping
+from cosmos.profiles import RedshiftUserPasswordProfileMapping
 
 profile_config = ProfileConfig(
     profile_name="default",
     target_name="dev",
-    profile_mapping=SnowflakeUserPasswordProfileMapping(
-        conn_id="snowflake_conn",   # configured in the Airflow Admin interface
-        profile_args={"database": "necars_db", "schema": "dbt_schema"},
+    profile_mapping=RedshiftUserPasswordProfileMapping(
+        conn_id="redshift_conn",   # configured in the Airflow Admin interface
+        profile_args={"dbname": "necars_db", "schema": "dbt_schema"},
     )
 )
 
 dbt_snowflake_dag = DbtDag(
-    project_config=ProjectConfig("/usr/local/airflow/dags/dbt/necars_dbt"),
+    project_config=ProjectConfig("/usr/local/airflow/dags/dbt/necars_dbt_rs"),
     operator_args={"install_deps": True},
     profile_config=profile_config,
     execution_config=ExecutionConfig(dbt_executable_path=f"{os.environ['AIRFLOW_HOME']}/dbt_dag_venv/bin/dbt"),
     schedule_interval="@daily",
-    start_date=datetime(2025, 3, 31),
+    start_date=datetime(2025, 5, 15),
     catchup=False,
     dag_id="necars_dbt_dag"
 )
